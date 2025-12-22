6af4e36d718f2215309848bd666bd99cda8cad8e
chLi <565353780@qq.com>
2025-12-22 19:43:11 +0800

    remove unused code

--- demo.py
+++ demo.py
@@ -1,6 +1,4 @@
-from uv_cube_gen.Demo.xyz_mapper import demo as demo_map_xyz
 from uv_cube_gen.Demo.camera import demo as demo_camera
 
 if __name__ == '__main__':
-    demo_map_xyz()
     demo_camera()
