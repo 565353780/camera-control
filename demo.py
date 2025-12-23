a21148e77b62ca93cd381e85bbb82b2060170fbc
chLi <565353780@qq.com>
2025-12-23 18:58:49 +0800

    add test diff render script

--- demo.py
+++ demo.py
@@ -1,4 +1,6 @@
 from camera_control.Demo.camera import demo as demo_camera
+from camera_control.Demo.nvdiffrast_renderer import demo as demo_diff_render
 
 if __name__ == '__main__':
-    demo_camera()
+    # demo_camera()
+    demo_diff_render()
