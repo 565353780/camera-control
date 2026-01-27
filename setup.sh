4f8e4f6b68b11552c48f541a5845784a03df0b7f
chLi <565353780@qq.com>
2026-01-27 13:23:57 +0800

    add colmap renderer module

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 cd ..
 git clone --depth 1 https://github.com/NVlabs/nvdiffrast.git
 
-pip install ninja trimesh open3d
+pip install ninja trimesh open3d viser
 
 pip3 install torch torchvision \
   --index-url https://download.pytorch.org/whl/cu124
