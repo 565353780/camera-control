7e274f611e1658df66a19fa80882d4bbe4991bd7
chLi <565353780@qq.com>
2026-06-13 11:55:48 +0800

    fix mark visible value error and test passed

--- mark_volume.py
+++ mark_volume.py
@@ -1,5 +1,9 @@
-from camera_control.Demo.volume_marker import demo_mark_volume
+from camera_control.Demo.volume_marker import (
+    demo_mark_glb,
+    demo_mark_real_data,
+)
 
 
 if __name__ == '__main__':
-    demo_mark_volume()
+    demo_mark_glb()
+    #demo_mark_real_data()
